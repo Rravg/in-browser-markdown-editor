00cb9dc7dc71367e39ece82b340ec5a35d065148
rodrigo <xravg.rodrigo@gmail.com>
2022-08-17 03:15:41 -0600

    deploy

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL: string = "https://online-markdown-editor-api.herokuapp.com";
+const BASE_URL: string = "http://54.146.132.56:8000";
 // const BASE_URL: string = "http://localhost:8000";
 const URL_API: string = `${BASE_URL}/api/v1/editor`;
 
