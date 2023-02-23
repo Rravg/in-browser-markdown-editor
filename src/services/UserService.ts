a1f1972191f31adde4c2e415edac0b1c9c6b4848
rodrigo <xravg.rodrigo@gmail.com>
2023-02-22 23:08:33 -0600

    update services

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,24 +3,25 @@ import axios from "../api/axios";
 const LOGIN_URL: string = "/login";
 const LOGOUT_URL: string = "/logout";
 const SIGNUP_URL: string = "/signup";
+const URL_STRING = "https://markdown-editor-backend.up.railway.app";
 
 export default class UserService {
     public static login(user: User) {
         return axios.post(
-            `https://online-markdown-editor-api.herokuapp.com/api/v1/editor${LOGIN_URL}`,
+            `${URL_STRING}/api/v1/editor${LOGIN_URL}`,
             user
         );
     }
 
     public static logout() {
         return axios.get(
-            `https://online-markdown-editor-api.herokuapp.com/api/v1/editor${LOGOUT_URL}`
+            `${URL_STRING}/api/v1/editor${LOGOUT_URL}`
         );
     }
 
     public static signup(user: User, date: string) {
         return axios.post(
-            `https://online-markdown-editor-api.herokuapp.com/api/v1/editor${SIGNUP_URL}`,
+            `${URL_STRING}/api/v1/editor${SIGNUP_URL}`,
             user,
             { params: { date: date } }
         );
